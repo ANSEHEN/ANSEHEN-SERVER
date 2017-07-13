66867d386938d2bb7610ba8def0b2ca36641388e
EHKIM10 <esguthaben@gmail.com>
2017-07-13 15:48:00 +0900

    server and db

---
+++ server.cpp
@@ -0,0 +1,81 @@
+#include "server.h"
+
+#define PORT 9001
+const char *host = (char*)"localhost";
+const char *user = (char*)"root";
+const char *pw = (char*)"bitiotansehen";
+const char *db = (char*)"ansehen";
+char    buffer[BUFSIZ];
+
+int main(void)
+{
+        int     c_socket, s_socket;
+        struct sockaddr_in s_addr, c_addr;
+        socklen_t       len;
+        int     n,query_stat;
+        MYSQL *connection;
+        MYSQL_RES  *sql_result;
+        MYSQL_ROW sql_row;
+        char query[BUFSIZ];
+
+        s_socket = socket(PF_INET, SOCK_STREAM, 0);
+
+        memset(&s_addr, 0, sizeof(s_addr));
+        s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        s_addr.sin_family = AF_INET;
+        s_addr.sin_port = htons(PORT);
+        ////////////////////////////////////////
+
+        /* db */
+        connection = mysql_init(NULL);
+        if(!mysql_real_connect(connection,host,user,pw,db,0,NULL,0))
+        {
+                fprintf(stderr,"%s\n",mysql_error(connection));
+		exit(1);
+        }
+
+        //send sql query
+        if(mysql_query(connection, "show tables"))
+        {
+                fprintf(stderr,"%s\n",mysql_error(connection));
+                exit(1);
+        }
+        sql_result = mysql_use_result(connection);
+
+        //output table name
+        printf("MySQL Tables in mysql database:\n");
+        while((sql_row=mysql_fetch_row(sql_result))!=NULL)
+                printf("%s\n",sql_row[0]);
+
+        mysql_free_result(sql_result);
+        /* network */
+        if(bind(s_socket, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) {
+                printf("Can not Bind\n");
+                return -1;
+        }
+
+        if(listen(s_socket, 5) == -1) {
+                printf("listen Fail\n");
+                return -1;
+        }
+le(1) {
+                len = sizeof(c_addr);
+                c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
+                n=read(c_socket, buffer,sizeof(buffer));
+                buffer[n]='\0';
+                printf("received Data : %s\n",buffer);
+                memset(query,'\0',strlen(query));
+                sprintf(query,"insert into USER_INFO (name,phone_num,image_add,start,end) values ('la','%s','123','111','111')",buffer);
+                query_stat = mysql_query(connection,query);
+                if(query_stat != 0)
+                {
+                        fprintf(stderr,"Mysql query error : %s\n",mysql_error(connection));
+                        return 1;
+                }
+                close(c_socket);
+        }
+        close(s_socket);
+        mysql_close(connection);
+        return 0;
+}
+
---
+++ server.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+#include <mysql.h>
+#include <stdlib.h>
+
