3016d5366345c0843dbd9e325cfb895ef5795e14
EHKIM10 <esguthaben@gmail.com>
2017-07-19 13:36:04 +0900

    add cctv_info

--- server.cpp
+++ server.cpp
@@ -7,8 +7,11 @@ const char *user = (char*)"root";
 const char *pw = (char*)"bitiotansehen";
 const char *db = (char*)"ansehen";
 char    buffer[BUFSIZ];
+int max_cctv;
 
+int CCTV::total_cctv=0;
 static size_t write_data(void *ptr,size_t size, size_t nmemb, void * stream);
+Node* cctv_info_load();
 
 int main(void)
 {
@@ -32,6 +35,7 @@ int main(void)
         ////////////////////////////////////////
 
         /* db */
+	Node * root = cctv_info_load();
         connection = mysql_init(NULL);
         if(!mysql_real_connect(connection,host,user,pw,db,0,NULL,0))
         {
@@ -139,3 +143,79 @@ static size_t write_data(void *ptr,size_t size, size_t nmemb, void * stream)
 	size_t written = fwrite(ptr, size, nmemb, (FILE *) stream);
 	return written;
 }
+
+Node* cctv_info_load()
+{
+        Node *root = new Node();
+        Node *cur =root;
+
+        int query_stat;
+        MYSQL *connection;
+        MYSQL_RES  *sql_result;
+        MYSQL_ROW sql_row;
+        char query[BUFSIZ];
+        char *ptr;
+
+
+        /* db */
+        connection = mysql_init(NULL);
+        if(!mysql_real_connect(connection,host,user,pw,db,0,NULL,0))
+        {
+                fprintf(stderr,"%s\n",mysql_error(connection));
+                exit(1);
+        }
+        //Query _ number of CCTV
+        if(mysql_query(connection, "select TABLE_ROWS from information_schema.tables where table_name = 'CCTV_INFO'"))
+        {
+                fprintf(stderr,"%s\n",mysql_error(connection));
+                exit(1);
+        }
+        sql_result = mysql_use_result(connection);
+        sql_row=mysql_fetch_row(sql_result);
+        max_cctv =atoi(sql_row[0]);
+        printf("max_cctv : %d\n",max_cctv);
+
+        mysql_free_result(sql_result);
+
+        //Query _ CCTV_INFO
+        if(mysql_query(connection, "select * from CCTV_INFO"))
+       {
+                fprintf(stderr,"%s\n",mysql_error(connection));
+                exit(1);
+        }
+        sql_result = mysql_use_result(connection);
+        int i=0;
+        while((sql_row=mysql_fetch_row(sql_result))!=NULL)
+        {
+                char *id=new char[5];
+                strcpy(id,sql_row[0]);
+                printf("%s      ",id);
+                char *b_id=new char[BUFSIZ];
+                strcpy(b_id,sql_row[1]);
+                printf("%s      ",b_id);
+                char * ip = new char[BUFSIZ];
+                strcpy(ip,sql_row[2]);
+                printf("%s      ",ip);
+                char * lo = new char[BUFSIZ];
+                strcpy(lo,sql_row[3]);
+                printf("%s\n",lo);
+                cur->data=new CCTV(id,b_id,ip,lo);
+                if(i!=max_cctv-1)
+                {
+                        cur->rear = new Node();
+                        cur->rear->front=cur;
+                        cur=cur->rear;
+                }
+                i++;
+
+        }
+
+
+        mysql_free_result(sql_result);
+
+        mysql_close(connection);
+	return root;
+}
+
+
+
