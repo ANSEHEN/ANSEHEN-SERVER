85a3218cd7fc906d86f00bc353928207682a6622
EHKIM10 <esguthaben@gmail.com>
2017-09-12 16:13:29 +0900

    multithread with socket

--- server.cpp
+++ server.cpp
@@ -10,25 +10,15 @@ const char *pw = (char*)"bitiotansehen";
 const char *db = (char*)"ansehen";
 char    buffer[BUFSIZ];
 int max_cctv;
-
-static size_t write_data(void *ptr,size_t size, size_t nmemb, void * stream);
-Node* cctv_info_load();
-void bcn_sig_to_cctv(int *msgid);
-void get_location(thread_data *data);
-void result_to_android(int *msgid);
+int msgid;
 
 int main(void)
 {
-        int     c_socket, s_socket;
+        int      s_socket;
         struct sockaddr_in s_addr, c_addr;
         socklen_t       len;
         int     n,query_stat;
-        MYSQL *connection;
-        MYSQL_RES  *sql_result;
-        MYSQL_ROW sql_row;
-        char query[BUFSIZ];
-		char *ptr;
-		char c_buff[ARG_MAX][BUFSIZ];
+        
         
 		s_socket = socket(PF_INET, SOCK_STREAM, 0);
 
@@ -37,35 +27,11 @@ int main(void)
         s_addr.sin_family = AF_INET;
         s_addr.sin_port = htons(PORT);
         ////////////////////////////////////////
+		int msgid;
+		msgid=msgget(1234,IPC_CREAT);
 
 		/* CCTV Information*/
 		Node * root = cctv_info_load();
-
-        /* db */
-
-        connection = mysql_init(NULL);
-        if(!mysql_real_connect(connection,host,user,pw,db,0,NULL,0))
-        {
-                fprintf(stderr,"%s\n",mysql_error(connection));
-				exit(1);
-        }
-
-        //send sql query
-        if(mysql_query(connection, "show tables"))
-        {
-                fprintf(stderr,"%s\n",mysql_error(connection));
-                exit(1);
-        }
-        sql_result = mysql_use_result(connection);
-
-        //output table name
-        printf("MySQL Tables in mysql database:\n");
-        while((sql_row=mysql_fetch_row(sql_result))!=NULL)
-                printf("%s\n",sql_row[0]);
-
-        mysql_free_result(sql_result);
-
-
 
         /* network */
         if(bind(s_socket, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) 
@@ -80,103 +46,133 @@ int main(void)
                 return -1;
         }
 	
-		int msgid;
-		msgid=msgget(1234,IPC_CREAT);
 
-		thread beaconSignaltoCCTV(&bcn_sig_to_cctv,&msgid);
-		thread resultSignaltoAndroid(&result_to_android,&msgid);
+		//thread beaconSignaltoCCTV(bcn_sig_to_cctv,&msgid);
+		//thread resultSignaltoAndroid(&result_to_android,&msgid);
+
+		vector<Thread> thr;
+		vector<int> c_socket;
+		int num=0;
+
 		while(1) 
 		{
             len = sizeof(c_addr);
-			printf("[main]before accepting c_socket\n");
-		 	mbuf msg;
-
-		    c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
-			printf("[main]after accepting c_socket\n");
-        	n=read(c_socket, buffer,sizeof(buffer));
-			buffer[strlen(buffer)+1]='\0';
-			printf("[main]got : %s\n",buffer);
-			ptr=strtok(buffer," ");
-
-			for(int i=0; i<ARG_MAX ;i++)
-			{
-				if(ptr==NULL)
-					break;
-				strcpy(c_buff[i],ptr);
-				ptr= strtok(NULL," ");
-				printf("[main]c_buff[%d] : %s\n",i,c_buff[i]);
-			}
-
-			sprintf(query,"insert into USER_INFO (phone_num,phone_num_input,name,pw,image_add,unique_key,path,end) values ('%s','%s','%s','%s','%s','%s','start', 'end')",c_buff[0],c_buff[1],c_buff[2],c_buff[3],c_buff[4],c_buff[5]);
-
-			query_stat = mysql_query(connection,query);
-            if(query_stat != 0)
-            {
- 		           fprintf(stderr,"Mysql query error : %s\n",mysql_error(connection));
-				   //duplicatekey
-                        return 1;
-            }
-
-			/* file download from url */
-			CURL *curl_handle;
-			char *pagefilename =c_buff[4];
-			char url[BUFSIZ];
-			sprintf(url,"http://13.124.164.203/image/%s",pagefilename); 
-			FILE *pagefile;
-			curl_global_init(CURL_GLOBAL_ALL);
-
-  			// init the curl session 
-  			curl_handle = curl_easy_init();
-
-	 		// set URL to get here 
-			curl_easy_setopt(curl_handle, CURLOPT_URL, url);
-
-			// Switch on full protocol/debug output while testing 
-			curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
-
-			// disable progress meter, set to 0L to enable and disable debug output 
-			curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
-
-			// send all data to this function  
-			curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
-
-			// open the file 
-			pagefile = fopen(pagefilename, "wb");
-			if(pagefile) 
-			{
-				// write the page body to this file handle 
-				curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
-				curl_easy_perform(curl_handle);
-
-				// close the header file 
-				fclose(pagefile);
-			}	
-
-			// cleanup curl stuff 
-			curl_easy_cleanup(curl_handle);
-            
-			
-			printf("[main] thread data create\n");
-			thread_data threadData(&s_socket,c_buff[5]);
-			printf("[main] thread create\n");
-			printf("[main] s_socket:%d\n",s_socket);
-			thread location_thr(get_location,&threadData);
-			location_thr.join();
-			//location_thr.detach();
-			printf("[main] after creating a thread\n");
-
-			close(c_socket);
-			//intercommunication with fileserver	
-			msg.mtype=1;
-			strcpy(msg.buf,"server sent\n");
-			strcpy(msg.unique_key,c_buff[5]);
-			strcpy(msg.image_addr,c_buff[4]);
-			if(msgsnd(msgid,(void*)&msg,sizeof(msg),0)==-1)
-             		perror("send fail ");
+			printf("[main] *%d. before accepting c_socket\n",num+1);
+		    int temp_c = accept(s_socket, (struct sockaddr *) &c_addr, &len);
+			c_socket.push_back(temp_c);
+			printf("[main] %d. after accepting c_socket\n",num+1);
+			Thread temp;
+			temp.c_socket_t=c_socket[num];
+			thr.push_back(temp);
+			thr[num].run();
+			printf("[main]running thr %d\n",num+1);		
+			num++;
         }
         close(s_socket);
-        mysql_close(connection);
         return 0;
+}
+
+void receive_data_from_android(int *csocket)
+{
+		mbuf msg;
+		int c_socket=*csocket;
+		MYSQL *connection;
+        MYSQL_RES  *sql_result;
+        MYSQL_ROW sql_row;
+        char query[BUFSIZ];
+		char *ptr;
+		char c_buff[ARG_MAX][BUFSIZ];
+		char s_check[10];
+		int query_stat;
+		printf("[receive data]\n");
+
+        connection = mysql_init(NULL);
+        if(!mysql_real_connect(connection,host,user,pw,db,0,NULL,0))
+        {
+                fprintf(stderr,"%s\n",mysql_error(connection));
+				exit(1);
+        }
+
+		printf("[receive_data]after accepting c_socket\n");
+        read(c_socket, buffer,sizeof(buffer));
+		strcpy(s_check,"zero");
+		//receive unique key from android
+		write(c_socket,s_check,strlen(s_check)+1);
+
+		buffer[strlen(buffer)+1]='\0';
+		printf("[receive_data]got : %s\n",buffer);
+		ptr=strtok(buffer," ");
+
+		for(int i=0; i<ARG_MAX ;i++)
+		{
+			if(ptr==NULL)
+				break;
+			strcpy(c_buff[i],ptr);
+			ptr= strtok(NULL," ");
+			printf("[receive_data]c_buff[%d] : %s\n",i,c_buff[i]);
+		}
+
+		sprintf(query,"insert into USER_INFO (phone_num,phone_num_input,name,pw,image_add,unique_key,path,end) values ('%s','%s','%s','%s','%s','%s','start', 'end')",c_buff[0],c_buff[1],c_buff[2],c_buff[3],c_buff[4],c_buff[5]);
+
+		query_stat = mysql_query(connection,query);
+        if(query_stat != 0)
+        {
+ 		    fprintf(stderr,"Mysql query error : %s\n",mysql_error(connection));
+        }
+
+            
+			
+		get_location(c_socket);
+
+		printf("[receive_data] after setting location\n");
+		
+		/* file download from url */
+		CURL *curl_handle;
+		char *pagefilename =c_buff[4];
+		char url[BUFSIZ];
+		sprintf(url,"http://13.124.164.203/image/%s",pagefilename); 
+		FILE *pagefile;
+		curl_global_init(CURL_GLOBAL_ALL);
+
+  		// init the curl session 
+  		curl_handle = curl_easy_init();
+
+	 	// set URL to get here 
+		curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+
+		// Switch on full protocol/debug output while testing 
+		curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+
+		// disable progress meter, set to 0L to enable and disable debug output 
+		curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+
+		// send all data to this function  
+		curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+
+		// open the file 
+		pagefile = fopen(pagefilename, "wb");
+		if(pagefile) 
+		{
+			// write the page body to this file handle 
+			curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
+			curl_easy_perform(curl_handle);
+
+			// close the header file 
+			fclose(pagefile);
+		}	
+
+		// cleanup curl stuff 
+		curl_easy_cleanup(curl_handle);
+
+		close(c_socket);
+		//intercommunication with fileserver	
+		msg.mtype=1;
+		strcpy(msg.buf,"server sent\n");
+		strcpy(msg.unique_key,c_buff[5]);
+		strcpy(msg.image_addr,c_buff[4]);
+		if(msgsnd(msgid,(void*)&msg,sizeof(msg),0)==-1)
+             perror("send fail ");
+        mysql_close(connection);
 }
 
 void result_to_android(int *msgid)
@@ -212,23 +208,22 @@ void result_to_android(int *msgid)
 	}
 }
 
-void get_location(thread_data *data)
+void get_location(int c_socket)
 {
-        int     c_socket;
         int query_stat;
         MYSQL *connection;
         MYSQL_RES  *sql_result;
         MYSQL_ROW sql_row;
         char query[BUFSIZ];
         char *ptr;
-        struct sockaddr_in  c_addr;
+        
 		char r_uniqueKey[50],r_endPoint[BUFSIZ],r_allPath[BUFSIZ];
 		char s_check[10];
-		int *s_socket=data->s_socket;
+		
 		Node *root;
-		socklen_t len = sizeof(c_addr);
+	
 		printf("[get location]\n");
-		printf("[get location]s_socket : %d\n",*s_socket);
+		
 		/* db */
         connection = mysql_init(NULL);
         if(!mysql_real_connect(connection,host,user,pw,db,0,NULL,0))
@@ -236,11 +231,6 @@ void get_location(thread_data *data)
                 fprintf(stderr,"%s\n",mysql_error(connection));
                 exit(1);
         }
-
-		/* network */
-	    c_socket = accept(*s_socket, (struct sockaddr *) &c_addr,&len);
-		printf("[get location] c_socket accepted\n");
-		printf("[get_location]uniqueKey : %s wait for location info\n",data->uni_key);
 
 		strcpy(s_check,"zero");
 		
@@ -326,17 +316,10 @@ void get_location(thread_data *data)
 				write(c_socket,s_check,strlen(s_check)+1);
 		}
 		write(c_socket,s_check,strlen(s_check)+1);
-		close(c_socket);
+		
 
         /* db */
-        connection = mysql_init(NULL);
-        if(!mysql_real_connect(connection,host,user,pw,db,0,NULL,0))
-        {
-                fprintf(stderr,"%s\n",mysql_error(connection));
-                exit(1);
-        }
 		sprintf(query,"update USER_INFO set end = '%s' where unique_key = '%s'",r_endPoint,r_uniqueKey);
-
 		query_stat = mysql_query(connection,query);
         if(query_stat != 0)
         {
@@ -516,5 +499,11 @@ Node* cctv_info_load()
 	return root;
 }
 
+void Thread::run()
+{
+		printf("[Thread class]\n");
+		thread thr(receive_data_from_android,&c_socket_t);
+		thr.detach();
+}
 
 
--- server.h
+++ server.h
@@ -10,6 +10,7 @@
 #include <sys/ipc.h>
 #include <iostream>
 #include <thread>
+#include <vector>
 #include "cctv.h"
 
 #define TYPE_FILE 1
@@ -17,7 +18,8 @@
 #define TYPE_BEACON_L 3
 #define TYPE_BEACON_C 4
 #define TYPE_RESULT 5
-class mbuf {
+class mbuf 
+{
 	public :
 	long mtype;
 	char buf[100];
@@ -26,29 +28,25 @@ class mbuf {
 	int result;
 };
 
-class beacon_data{
+class beacon_data
+{
 	public:
 	long mtype;
 	int state;
 	char BeaconId[30];
 	char PrimaryKey[100];
 };
-class thread_data{
+class Thread
+{
 		public:
-		int *s_socket;
-		char *uni_key;
-		thread_data(){};
-		thread_data(int *s,char *u)
-		{
-			s_socket=s;
-			uni_key=new char[strlen(u)+1];
-			strcpy(uni_key,u);
-			printf("[thread_data]u:%s\n",u);
-			printf("[thread_data]uni_key:%s\n",uni_key);
-		};
-		~thread_data()
-		{
-				delete uni_key;
-
-		}
+		int c_socket_t;
+		void run();
 };
+
+
+static size_t write_data(void *ptr,size_t size, size_t nmemb, void * stream);
+Node* cctv_info_load();
+void bcn_sig_to_cctv(int *msgid);
+void get_location(int c_socket);
+void result_to_android(int *msgid);
+void receive_data_from_android(int *csocket);
